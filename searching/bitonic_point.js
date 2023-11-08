766bb8eaa096e7345368ce4c9672f853897f2119
Harsh Haria <harshharia1234@gmail.com>
2023-11-09 00:01:23 +0530

    example added for bitonic point

--- searching/bitonic_point.js
+++ searching/bitonic_point.js
@@ -3,6 +3,13 @@
  * @description Given an array arr of n elements that is first strictly increasing and then maybe strictly decreasing, find the maximum element in the array.
 Note: If the array is increasing then just print the last element will be the maximum value.
  * 
+ * @example
+ * Input:
+ * - n = 9
+ * - arr[] = {1,15,25,45,42,21,17,12,11}
+ * - Output: 45
+ * - Explanation: Maximum element is 45.
+ * 
  * @param {number[]} arr
  * @param {number} N
  * @returns {number}
