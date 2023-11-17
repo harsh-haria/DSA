16f9219f8bdbd0cbb4f2aa5943569040dc79d63f
Harsh Haria <harshharia1234@gmail.com>
2023-11-18 00:00:37 +0530

    added new program to determine of two elememts add up to a number X in O(N)

---
+++ hashing/key_pair_easy.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @description Given an array Arr of N positive integers and another number X. Determine whether or not there exist two elements in Arr whose sum is exactly X.
+ * 
+ * @param {number[]} arr
+ * @param {number} n
+ * @param {number} x
+ * @return {boolean}
+ */
+
+class Solution {
+    hasArrayTwoCandidates(arr, n, x) {
+        //code here
+        let elementMap = new Map();
+        for (let item of arr) {
+            elementMap.set(item, item);
+        }
+        for (let item of arr) {
+            let fetchInt = x - item;
+            if (elementMap.has(fetchInt)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
