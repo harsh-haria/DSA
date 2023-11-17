23dc0612c70b2b1f5a876651908e7b692531125c
Harsh Haria <harshharia1234@gmail.com>
2023-11-18 00:51:35 +0530

    updated the program key_pair_easy. bug fix and added link.

--- hashing/key_pair_easy.js
+++ hashing/key_pair_easy.js
@@ -2,6 +2,8 @@
  * 
  * @description Given an array Arr of N positive integers and another number X. Determine whether or not there exist two elements in Arr whose sum is exactly X.
  * 
+ * @link https://practice.geeksforgeeks.org/problems/key-pair5616/1
+ * 
  * @param {number[]} arr
  * @param {number} n
  * @param {number} x
@@ -12,14 +14,16 @@ class Solution {
     hasArrayTwoCandidates(arr, n, x) {
         //code here
         let elementMap = new Map();
-        for (let item of arr) {
-            elementMap.set(item, item);
-        }
+        // for (let item of arr) {
+        //     elementMap.set(item, item);
+        // }
         for (let item of arr) {
             let fetchInt = x - item;
+            // if(item == fetchInt) continue;
             if (elementMap.has(fetchInt)) {
                 return true;
             }
+            elementMap.set(item, item);
         }
         return false;
     }
