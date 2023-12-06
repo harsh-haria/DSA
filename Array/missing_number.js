bda3c4da7a59ae40f6d3eb2abeadc95e91bc26f7
Harsh Haria <harshharia1234@gmail.com>
2023-12-06 20:56:03 +0530

    added link in array/missing_number.js program

--- Array/missing_number.js
+++ Array/missing_number.js
@@ -4,6 +4,8 @@
  * 
  * @description Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
  * 
+ * @link https://practice.geeksforgeeks.org/problems/missing-number-in-array1416/1
+ * 
  * @param {number[]} array
  * @param {number} n
  * @returns {number}
