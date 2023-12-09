0b1015387c89935dbc36a0b757b36a61408ee6ab
Harsh Haria <harshharia1234@gmail.com>
2023-12-09 23:52:29 +0530

    added desc for diagonal sort

--- matrix/sorting_diagonally_easy.js
+++ matrix/sorting_diagonally_easy.js
@@ -1,4 +1,6 @@
 /**
+ * @description Given an NxM 2D matrix, rearrange such that. Each diagonal in the lower left triangle of the rectangular grid is sorted in ascending order. Each diagonal in the upper right triangle of the rectangular grid is sorted in descending order. The major diagonal in the grid starting from the top-left corner is not rearranged. 
+ * 
  * @link https://practice.geeksforgeeks.org/problems/diagonal-morning-assembly0028/1
  * 
  * @param {number[][]} matrix
