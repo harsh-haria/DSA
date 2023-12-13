ec41bb3addb2d84d58c201eb24e56d9c3a3f6c7d
Harsh Haria <harshharia1234@gmail.com>
2023-12-14 01:11:09 +0530

    added changes for the first_neg_ele in window. implemented but not accepted TLE

--- queue/first_neg_int_queue_easy.js
+++ queue/first_neg_int_queue_easy.js
@@ -17,6 +17,10 @@ class Queue {
     peek() {
         return this.items[this.frontIndex];
     }
+    lengther(){
+        let size = Object.keys(this.items).length;
+        return size;l
+    }
     get printQueue() {
         return this.items;
     }
@@ -35,10 +39,34 @@ class Solution {
         // code here
         let queue = new Queue();
         let ans = [];
-        
-        for(let item of arr){
-            if(k)
-            queue.enqueue(item);
+        let idx = 0;
+
+        let i=0, j=0;
+        while(j<n){
+            if(arr[j]<0){
+                queue.enqueue(arr[j]);
+            }
+            if(j-i+1 == k){
+                if(queue.lengther() === 0){
+                    ans[idx++] = 0;
+                }
+                else{
+                    ans[idx++] = queue.peek();
+                    if(arr[i] === queue.peek()){
+                        queue.dequeue();
+                    }
+                }
+                i++;
+            }
+            j++;
         }
+        return ans;
     }
 }
+
+const solution = new Solution();
+let n = 5;
+let k = 2;
+let array = [-8,2,3,-6,10];
+let answer = solution.printFirstNegativeInteger(n,k,array);
+console.log(answer);
